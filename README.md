acf24bc0f01ac33bc72f0f00315ae482c4e71b13
Renan Lima <renanlimabl@gmail.com>
2021-01-29 20:44:46 -0300

    add metodo para fazer o fetch no lado do servidor

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * styled-components [x]
 * server fake [npx json-server server.json -p 3333 -w] [x]
 * client side fetch [x]
+* server fake com delay 2s [npx json-server server.json -p 3333 -w -d 2000] [x]
+* server side fetch [x]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
 interface IProduct {
@@ -6,16 +7,20 @@ interface IProduct {
   title: string;
 }
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+interface IHomeProps {
+  recommendedProducts: IProduct[];
+}
 
-  useEffect(() => {
-    fetch('http://localhost:3333/recommended').then(response => {
-      response.json().then(data => {
-        setRecommendedProducts(data)
-      })
-    })
-  }, [])
+export default function Home({ recommendedProducts }: IHomeProps) {
+
+  // const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/recommended').then(response => {
+  //     response.json().then(data => {
+  //       setRecommendedProducts(data)
+  //     })
+  //   })
+  // }, [])
 
   return (
     <div>
@@ -33,4 +38,15 @@ export default function Home() {
       </section>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts
+    }
+  }
 }
